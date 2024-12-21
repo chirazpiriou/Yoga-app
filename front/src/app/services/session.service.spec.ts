d28f746eac734b907802cb006362c8980bde3d72
chiraz.piriou <chirazpiriou@gmail.com>
2024-12-21 17:04:48 +0100

    Add tests for SessionService front

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -1,17 +1,82 @@
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
-
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 import { SessionService } from './session.service';
+import { BehaviorSubject } from 'rxjs';
+import {  Observable } from 'rxjs';
 
 describe('SessionService', () => {
-  let service: SessionService;
+  let sessionService: SessionService;
+
+  // Mock data conforming to the SessionInformation interface
+  const mockSession: SessionInformation = {
+    token: 'Token1548795',
+    type: 'Bearer',
+    id: 1,
+    username: 'sarah@gmail.com',
+    firstName: 'sarah',
+    lastName: 'Dupuis',
+    admin: true
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(SessionService);
+    sessionService = TestBed.inject(SessionService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('SessionService Initial State', () => {
+    it('should be created', () => {
+      expect(sessionService).toBeTruthy();
+    });
+
+    it('should init  set isLogged to false', () => {
+      expect(sessionService.isLogged).toBeFalsy();
+    });
+
+    it('should init set sessionInformation to undefined', () => {
+      expect(sessionService.sessionInformation).toBeUndefined();
+    });
   });
+
+  describe('$isLogged', () => {
+    it('should return an Observable for $isLogged()', () => {
+      expect(sessionService.$isLogged()).toBeInstanceOf(Observable);
+    });
+  });
+
+  describe('logIn', () => {
+    it('should set isLogged to true and store session information', () => {
+      sessionService.logIn(mockSession);
+
+      expect(sessionService.isLogged).toBe(true); // Check that user is logged in
+      expect(sessionService.sessionInformation).toEqual(mockSession); // Check that session information is set
+    });
+
+    it('should emit true on $isLogged observable after login', (done) => {
+      sessionService.$isLogged().subscribe((isLogged) => {
+        expect(isLogged).toBe(true); // Ensure observable emits true after login
+        done();
+      });
+
+      sessionService.logIn(mockSession); // Call logIn to trigger observable emission
+    }); 
+  });
+
+  describe('logOut', () => {
+    it('should set isLogged to false and clear session information', () => {
+      sessionService.logOut();
+      expect(sessionService.isLogged).toBe(false);
+      expect(sessionService.sessionInformation).toBeUndefined();
+    });
+  });
+
+
+
+
+
+
+
+
+
+
 });
