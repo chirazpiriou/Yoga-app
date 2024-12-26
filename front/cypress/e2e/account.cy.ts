f44a66336e869490b8bc9433a2e4ac2d03852134
chiraz.piriou <chirazpiriou@gmail.com>
2024-12-27 00:09:39 +0100

    add e2e test for account.cy.ts

--- front/cypress/e2e/account.cy.ts
+++ front/cypress/e2e/account.cy.ts
@@ -1,5 +1,64 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('account-me not admin  spec', () => {
+  it("should login it is essential to be able to see the conncected user's info ", () => {
+    cy.visit('/login')
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        id: 3,
+        username: 'aline@gmail.com',
+        lastName: 'aline',
+        firstName: 'dupuis',
+        admin: false
+      },
+    }).as('login')
+    cy.intercept('GET','/api/session',[]).as('session')
+    cy.get('input[formControlName=email]').type("aline@gmail.com")
+    cy.get('input[formControlName=password]').type(`${"chats1547"}{enter}{enter}`)
+    cy.url().should('include', '/sessions')
   })
+
+  it('should show account user not admin information', () => {
+
+    cy.intercept('GET', '/api/user/3', {
+      body: [{
+              id: 3,
+              email: 'aline@gmail.com',
+              lastName: 'aline',
+              firstName: 'dupuis',
+              admin: false,
+              createdAt: '2024-12-20T00:00:00',
+              updatedAt: '2024-12-26T00:00:00',
+      }]
+    }).as('user1')
+
+
+    cy.get('span[data-cy=goToAccount]').click()
+    cy.url().should('include', '/me')
+    cy.wait('@user1');
+    cy.get('button[data-cy=deleteMeButton]').should('exist')
+  })
+
+  it('should delete account when click on delete and and redirect to the session page ', () => {
+
+    cy.intercept('GET', '/api/user/3', {
+      body: [{
+              id: 3,
+              email: 'aline@gmail.com',
+              lastName: 'aline',
+              firstName: 'dupuis',
+              admin: false,
+              createdAt: '2024-12-20T00:00:00',
+              updatedAt: '2024-12-26T00:00:00',
+      }]
+    }).as('user1')
+    cy.intercept('DELETE', '/api/user/3', {statusCode: 200})
+    cy.intercept('GET', '/api/session', {});
+    cy.url().should('include', '/me');
+    cy.get('button[data-cy=deleteMeButton]').click()
+    cy.get('span[data-cy=login]').should('exist')
+    cy.get('span[data-cy=register]').should('exist')
+  })
+
+
+
 })
+
