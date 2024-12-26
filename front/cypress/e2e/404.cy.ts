74e898b9d20d8f033e95a7bd1b3f6ab5f65760d1
chiraz.piriou <chirazpiriou@gmail.com>
2024-12-27 00:40:31 +0100

    add e2e test for 404.cy.ts

--- front/cypress/e2e/404.cy.ts
+++ front/cypress/e2e/404.cy.ts
@@ -1,5 +1,7 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('404 not found', () => {
+  it('Should show the 404 page when the page does not exist', () => {
+    cy.visit('/doesntexist');
+
+    cy.get('h1[data-cy=notFound]').should('contain', 'Page not found !');
+  });
 })
